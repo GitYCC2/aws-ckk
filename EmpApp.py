6f8b47bb314965656164337465967334470f4fed
GitYCC2 <93185619+GitYCC2@users.noreply.github.com>
2022-05-07 11:01:34 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -24,10 +24,12 @@ table = 'employee'
 @app.route("/deleteemp", methods=['POST'])
 def DeleteEmp():
     emp_id = request.form['emp_id']
-    emp_file = request.form['emp_file']
-    #temp_file = request.form['emp_file']
-    #split_file = temp_file.split('/')
-    #emp_file = split_file[3]
+    #emp_file = request.form['emp_file']
+    temp_file = request.form['emp_file']
+    split_file = temp_file.split('/')
+    emp_file = split_file[3]
+    print(emp_file)
+    
     cursor = db_conn.cursor()
     try: 
         s3 = boto3.resource('s3')

f0adfaa0f864c277d778ea67ae97e00e2a695fee
GitYCC2 <93185619+GitYCC2@users.noreply.github.com>
2022-05-07 14:57:37 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -79,7 +79,7 @@ def EditEmp():
     last_name = request.form['last_name']
     pri_skill = request.form['pri_skill']
     location = request.form['location']
-    emp_file = request.files['emp_image_file']
+    emp_image_file = request.files['emp_image_file']
     
     update_sql = "UPDATE employee SET first_name=%s, last_name=%s, pri_skill=%s, location=%s WHERE emp_id=%s"
     cursor = db_conn.cursor()
